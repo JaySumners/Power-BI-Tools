3dc52ccd1d38481b7d7fdbb619b15c18f8fd4d72
Jay Sumners <55517453+JaySumners@users.noreply.github.com>
2019-10-17 14:33:03 -1000

    Add files via upload

---
+++ Power BI on Powershell/Get Pro License Users.ps1
@@ -0,0 +1,35 @@
+﻿
+## Microsoft Azure Active Directory Module for Windows PowerShell
+#Install-Module MSOnline
+#Use -Force to override if you are updating
+
+# Set function to get path later on
+Function Get-Folder($initialDirectory)
+{
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms")|Out-Null
+
+    $foldername = New-Object System.Windows.Forms.FolderBrowserDialog
+    $foldername.Description = "Select a folder"
+    $foldername.rootfolder = "MyComputer"
+
+    if($foldername.ShowDialog() -eq "OK")
+    {
+        $folder += $foldername.SelectedPath
+    }
+    return $folder
+}
+
+#NOTE: Do not need to run in admin.
+Connect-MsolService
+
+# All users with a pro license (will take some time to run)
+$proUsers = Get-MsolUser -All | 
+            Where-Object {$_.Licenses.AccountSkuId -eq "discoverycomm:POWER_BI_PRO"} | 
+            Select-Object -Property UserPrincipalName, DisplayName, FirstName, LastName, Title, Department, Office, Country, BlockCredential
+
+#No Disconnect for MsolService
+
+## Get JSON data on Power BI Usage
+$path = Get-Folder
+
+$proUsers | Export-Csv -Path $path -NoTypeInformation
